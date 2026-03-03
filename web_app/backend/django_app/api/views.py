c3ffb0918549654415b5e66c92693fc0d91bb462
Lemetist <lemetist@gmail.com>
2026-03-04 00:06:13 +0300

    feat: добавлена сериализация

--- web_app/backend/django_app/api/views.py
+++ web_app/backend/django_app/api/views.py
@@ -2,32 +2,30 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth import authenticate
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from django.contrib.auth.models import User
+
+from .serializers import RegisterSerializer, UserSerializer
+
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def echo(request):
-	return Response({"received": request.data})
+    return Response({"received": request.data})
 
 
 @api_view(["POST"])
 @permission_classes([AllowAny])
 def register(request):
-	username = request.data.get("username")
-	password = request.data.get("password")
-	if not username or not password:
-		return Response({"detail": "username and password required"}, status=status.HTTP_400_BAD_REQUEST)
+    serializer = RegisterSerializer(data=request.data)
 
-	if User.objects.filter(username=username).exists():
-		return Response({"detail": "user already exists"}, status=status.HTTP_400_BAD_REQUEST)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-	User.objects.create_user(username=username, password=password)
-	return Response({"detail": "registered"}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def me(request):
-	return Response({"username": request.user.username})
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
